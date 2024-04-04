35ffcc6bace0ac66181396124c02bf2305cb7b8b
kilim <s28346@pjwstk.edu.pl>
2024-04-04 18:08:51 +0200

    fwsf

---
+++ zadanie/LegacyApp/UserService.cs
@@ -0,0 +1,88 @@
+﻿using System;
+
+namespace LegacyApp
+{
+    public class UserService
+    {
+        private readonly IClientRepository _clientRepository;
+        private readonly IUserCreditService _userCreditService;
+
+        public UserService(IClientRepository clientRepository, IUserCreditService userCreditService)
+        {
+            _clientRepository = clientRepository;
+            _userCreditService = userCreditService;
+        }
+        public bool AddUser(string firstName, string lastName, string email, DateTime dateOfBirth, int clientId)
+        {
+            if (!IsValidUserInput(firstName, lastName, email, dateOfBirth)) return false;
+
+            var client = _clientRepository.GetById(clientId);
+            var user = CreateUser(firstName, lastName, email, dateOfBirth, client);
+
+            if (ShouldAssignCreditLimit(client.Type))
+            {
+                int creditLimit = CalculateCreditLimit(client.Type, lastName, dateOfBirth);
+                if (creditLimit < 500) return false;
+
+                user.HasCreditLimit = true;
+                user.CreditLimit = creditLimit;
+            }
+
+            UserDataAccess.AddUser(user);
+            return true;
+        }
+
+        private User CreateUser(string firstName, string lastName, string email, DateTime dateOfBirth, Client client)
+        {
+            return new User
+            {
+                FirstName = firstName,
+                LastName = lastName,
+                EmailAddress = email,
+                DateOfBirth = dateOfBirth,
+                Client = client,
+                // Initialize other properties as necessary
+            };
+        }
+
+        private bool ShouldAssignCreditLimit(string clientType)
+        {
+            return clientType != "VeryImportantClient";
+        }
+
+        private bool IsValidUserInput(string firstName, string lastName, string email, DateTime dateOfBirth)
+        {
+            if (string.IsNullOrEmpty(firstName) || string.IsNullOrEmpty(lastName))
+            {
+                return false;
+            }
+
+            if (!email.Contains("@") || !email.Contains("."))
+            {
+                return false;
+            }
+
+            var age = DateTime.Now.Year - dateOfBirth.Year;
+            if (dateOfBirth > DateTime.Now.AddYears(-age)) age--;
+
+            if (age < 21)
+            {
+                return false;
+            }
+
+            return true;
+        }
+        private int CalculateCreditLimit(string clientType, string lastName, DateTime dateOfBirth)
+        {
+            var baseCreditLimit = _userCreditService.GetCreditLimit(lastName, dateOfBirth);
+
+            if (clientType == "ImportantClient")
+            {
+                return baseCreditLimit * 2;
+            }
+
+            return baseCreditLimit;
+        }
+
+    }
+}
---
+++ zadanie/LegacyAppConsumer/Program.cs
@@ -0,0 +1,15 @@
+﻿using LegacyApp;
+using System;
+
+namespace LegacyAppConsumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var clientRepository = new ClientRepository();
+            var userCreditService = new UserCreditService();
+            var userService = new UserService(clientRepository, userCreditService);
+        }
+    }
+}
